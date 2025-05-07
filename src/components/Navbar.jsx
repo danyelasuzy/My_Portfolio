b0dfaa63c76fe0e0ffdf41744ea71b5091a3f596
Daniela Susan <elenadaniela26@yahoo.com>
2025-05-07 16:21:18 +0200

    fixes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,54 +10,40 @@ export const Navbar = ({ menuOpen, setMenuOpen }) => {
   }, [menuOpen]);
 
   return (
-    <nav className="fixed top-4 w-full md:w-3/4 mx-auto   z-40 backdrop-blur-lg border-b border-white/10 shadow-lg">
-      <div className="max-w-screen-lg justify-between ">
-        <div className="flex justify-between items-center h-16">
-          <a href="#home" className="font-mono text-xl font-bold text-white">
-            <img
-              src={`${import.meta.env.BASE_URL}susan-logo.png`}
-              alt="Susan Logo"
-              className="max-w-[120px] md:max-w-[160px]"
-            />
+    <nav className="fixed top-0 left-0 right-0 z-40 backdrop-blur-lg px-4 p-2 shadow-md border-b border-white/10">
+      <div className="flex items-center justify-between max-w-screen-lg mx-auto h-16 w-full">
+        <a href="#home">
+          <img
+            src={`${import.meta.env.BASE_URL}susan-logo.png`}
+            alt="Susan Logo"
+            className="max-w-[120px] md:max-w-[160px] mt-4 md:mt-5"
+          />
+        </a>
+
+        {/* Hamburger Icon */}
+        <div
+          className="w-6 h-5 flex flex-col justify-between cursor-pointer md:hidden"
+          onClick={() => setMenuOpen((prev) => !prev)}
+        >
+          <span className="block w-full h-0.5 bg-white"></span>
+          <span className="block w-full h-0.5 bg-white"></span>
+          <span className="block w-full h-0.5 bg-white"></span>
+        </div>
+
+        {/* Desktop Menu */}
+        <div className="hidden md:flex items-center space-x-10">
+          <a href="#home" className="text-gray-300 hover:text-white">
+            Home
           </a>
-
-          {/* Hamburger Icon for Mobile */}
-          <div
-            className="w-6 h-5 flex flex-col justify-between cursor-pointer z-40 md:hidden"
-            onClick={() => setMenuOpen((prev) => !prev)}
-          >
-            <span className="block w-5 h-0.5 bg-white"></span>
-            <span className="block w-5 h-0.5 bg-white"></span>
-            <span className="block w-5 h-0.5 bg-white"></span>
-          </div>
-
-          {/* Main Links for Desktop */}
-          <div className="hidden md:flex items-center space-x-10 animate-vertical">
-            <a
-              href="#home"
-              className="text-gray-300 hover:text-white transition-colors"
-            >
-              Home
-            </a>
-            <a
-              href="#about"
-              className="text-gray-300 hover:text-white transition-colors"
-            >
-              About
-            </a>
-            <a
-              href="#projects"
-              className="text-gray-300 hover:text-white transition-colors"
-            >
-              Projects
-            </a>
-            <a
-              href="#contact"
-              className="text-gray-300 hover:text-white transition-colors"
-            >
-              Contact
-            </a>
-          </div>
+          <a href="#about" className="text-gray-300 hover:text-white">
+            About
+          </a>
+          <a href="#projects" className="text-gray-300 hover:text-white">
+            Projects
+          </a>
+          <a href="#contact" className="text-gray-300 hover:text-white">
+            Contact
+          </a>
         </div>
       </div>
     </nav>
